7819a7e41fbe849a47dd35b1c27d4fcaf9327081
Siyad Savad <siyadsavad313@gmail.com>
2025-11-24 21:00:25 +0530

    index view added + url added + navbar and index page created in templates folder

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+from .models import Product, Order
+
+
+
+
+def index(request):
+  products = Product.objects.all()
+  orders = Order.objects.filter(user=request.user, paid=True) if request.user.is_authenticated else []
+
+  context = {
+    "products": products,
+    "orders": orders,
+    "stripe_public_key": settings.STRIPE_PUBLIC_KEY
+  }
+  return render(request, "index.html", context)
