6a908dd45f6474a1260498078b060263ef482cb3
Siyad Savad <siyadsavad313@gmail.com>
2025-11-24 19:42:26 +0530

    models added for the product and order and use the django's user model for user

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  image = models.ImageField(upload_to="product_images/", null=True, blank=True)
+
+  def __str__(self):
+    return self.name
+
+
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  quantity = models.PositiveIntegerField()
+  total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+  stripe_session_id = models.CharField(max_length=200, unique=True)
+  paid = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return f"{self.product.name} - {self.user.username}"
+  
+
+
